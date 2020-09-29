098ebd00bad6262b2edc6cc0544858cd477b018a
johncabang <johncabz@gmail.com>
2020-09-29 00:05:49 +0000

    Added a GET Route to show the form

--- express_server.js
+++ express_server.js
@@ -14,21 +14,17 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
-
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 app.listen(PORT, () => {
