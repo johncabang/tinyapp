486523caba28d16ee21d48b9f4784e2dfe833afb
johncabang <johncabz@gmail.com>
2020-09-29 00:54:29 +0000

    Created a random string function

--- express_server.js
+++ express_server.js
@@ -2,7 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
+
+function generateRandomString() {
+  let randomString = Math.random().toString(36).substring(2, 8);
+  return randomString;
+};
+
+console.log(generateRandomString());
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -25,6 +35,11 @@ app.get('/urls/:shortURL', (req, res) => {
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body); // Log the Post request body to the console
+  res.send('Ok');        // Respont with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
