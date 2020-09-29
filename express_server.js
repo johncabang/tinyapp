8b45f5aee91f5f1e4635b0459fcdfc935e97c958
johncabang <johncabz@gmail.com>
2020-09-29 19:30:43 +0000

    Client-side update, form with two elements - text and button & added an edit column on the urls_index page

--- express_server.js
+++ express_server.js
@@ -55,7 +55,9 @@ app.get('/u/:shortURL', (req, res) => {
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
-})
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
