1bd6ee9a183e2d66675928ec753762fc80302207
johncabang <johncabz@gmail.com>
2020-10-01 23:17:49 +0000

    Updated permission features

--- express_server.js
+++ express_server.js
@@ -29,20 +29,35 @@ const verifyPassword = function(userDatabase, email, password) {
   const userFound = verifyEmail(userDatabase, email);
   if (userFound && userFound.password === password) {
     return userFound;
-  } 
+  }
   return false;
 };
 
+// Returns URL, where the userID = the logged-in user
+const urlsForUser = (userDatabase, users) => {
+  const results = {};
+  for (let key in userDatabase) {
+    const urlObj = userDatabase[key];
+    if (urlObj.userID === users) {
+      results[key] = urlObj;
+    } 
+  }
+  // console.log(results)
+  return results;
+};
 
+// urlsForUser(urlDatabase, 'abcd')
 
 const urlDatabase = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.ca'
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "abc34d" },
+  s34edr: { longURL: "https://www.yahoo.ca", userID: "f43faf" }
+
 };
 
 const users = {
   'userRandomID': {
-    id: 'userRandomID',
+    id: 'aJ48lW',
     email: 'user@example.com',
     password: 'purple-monkey-dinosaur'
   },
@@ -55,9 +70,10 @@ const users = {
 
 // Urls = key
 app.get('/urls', (req, res) => {
+  // console.log(urlDatabase['b6UTxQ'].longURL);
   const templateVars = {
     user: users[req.cookies["user_id"]],
-    urls: urlDatabase
+    urls: urlsForUser(urlDatabase, req.cookies["user_id"])
   };
   res.render('urls_index', templateVars);
 });
@@ -66,8 +82,13 @@ app.get('/urls', (req, res) => {
 app.get('/urls/new', (req, res) => {
   const templateVars = {
     user: users[req.cookies["user_id"]],
+    ursl: urlDatabase
+    // urls: urlsForUser(urlDatabase, req.cookies["user_id"])
   };
-  res.render('urls_new', templateVars);
+  if (templateVars.user) {
+    res.render('urls_new', templateVars);
+  } else
+    res.redirect('/login');
 });
 
 // Displays a single URL and its shortened form
@@ -75,14 +96,14 @@ app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {
     user: users[req.cookies["user_id"]],
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL].longURL
   };
   res.render('urls_show', templateVars);
 });
 
 // Redirect shortURL to longURL
 app.get('/u/:shortURL', (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL].long;
   res.redirect(longURL);
 });
 
@@ -104,11 +125,16 @@ app.get('/login', (req, res) => {
 
 // Post route, form submission - creates a new shortURL, add to database, redirects to /urls/:shortURL
 app.post('/urls', (req, res) => {
-  const shortURL = generateRandomString();
-  // console.log(req.body); // Log the Post request body to the console
-  // shortURL-longURL key-value pair saved to urlDatabase
-  urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`);
+  if (req.cookies["user_id"]) {
+    const shortURL = generateRandomString();
+    urlDatabase[shortURL] = { 
+      longURL: req.body.longURL,
+      userID: req.cookies["user_id"] 
+    };
+    res.redirect(`/urls/${shortURL}`);
+  } else {
+    res.redirect('/login');
+  }
 });
 
 // Registration page
@@ -130,7 +156,7 @@ app.post('/register', (req, res) => {
       password: req.body.password
     };
     res.cookie('user_id', userId);
-    console.log(users[userId].email)
+    console.log(users[userId].email);
     console.log(users[userId]);
     res.redirect('/urls');
   }
@@ -142,14 +168,14 @@ app.post('/login', (req, res) => {
   // let user = verifyEmail(users, email);
   let userPassword = verifyPassword(users, email, password);
   // console.log('***' + user);
-  console.log('^^^' + JSON.stringify(userPassword))
+  console.log('^^^' + JSON.stringify(userPassword));
 
   if (userPassword) {
     res.cookie('user_id', userPassword.id);
     res.redirect('/urls');
   } else {
     res.status(403);
-    res.send('Email and password is not valid')
+    res.send('Email and password is not valid');
   }
 });
 
@@ -162,8 +188,12 @@ app.post('/logout', (req, res) => {
 // Update
 app.post('/urls/:shortURL/update', (req, res) => {
   // console.log(req.body.updatedLongURL)
-  let shortURL = req.params.shortURL;
-  urlDatabase[shortURL] = req.body.updatedLongURL;
+    let shortURL = req.params.shortURL;
+    urlDatabase[shortURL] = { 
+      longURL: urlDatabase[req.params.shortURL],
+      userID: req.cookies["user_id"] 
+    };
+    console.log('***    ' + urlDatabase[req.params.shortURL])
   res.redirect(`/urls`);
 });
 
